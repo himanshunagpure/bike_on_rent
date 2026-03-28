c8076d8202f9796e510a9fd5a5f111f7892aa71d
Himanshu <nagpurehimanshu9@gmail.com>
2026-03-28 18:13:35 +0530

    debug2

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async() => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MOGODB CONNECTED");
-        console.log("testing")
+        console.log("testing2")
     } catch (error){
         console.error(error);
         process.exit(1);
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,26 +1,50 @@
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (to, subject, text, htmlContent) => {
+//   try {
+//     console.log("Sending email to:", to);
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//       }
+//     });
+//     await transporter.sendMail({
+//       from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//       html: htmlContent
+//     });
+//     console.log("Email sent successfully");
+//   } catch (err) {
+//     console.error("sendEmail FAILED:", err.message);
+//   }
+// };
+
+// export default sendEmail;
+
 import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, text, htmlContent) => {
-  try {
-    console.log("Sending email to:", to);
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
-    await transporter.sendMail({
-      from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html: htmlContent
-    });
-    console.log("Email sent successfully");
-  } catch (err) {
-    console.error("sendEmail FAILED:", err.message);
-  }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
+    to: to,
+    subject: subject,
+    text: text,
+    html: htmlContent
+  });
+
 };
 
 export default sendEmail;
