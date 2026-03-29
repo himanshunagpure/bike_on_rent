039cc23ff38b3fa3f68d45bcb665cf43b978d4dd
Himanshu <nagpurehimanshu9@gmail.com>
2026-03-29 15:50:57 +0530

    fix sendEmail missing import and export

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -51,16 +51,52 @@
 
 
 
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (to, subject, text, htmlContent) => {
+//   try {
+//     console.log("Sending email to:", to);
+//     console.log("EMAIL_USER:", process.env.EMAIL_USER);
+//     console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//       }
+//     });
+
+//     await transporter.sendMail({
+//       from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
+//       to: to,
+//       subject: subject,
+//       text: text,
+//       html: htmlContent
+//     });
+
+//     console.log("Email sent successfully to:", to);
+
+//   } catch (err) {
+//     console.error("sendEmail FAILED:", err.message);
+//   }
+// };
+
+// export default sendEmail;
+
+
+
+
 import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, text, htmlContent) => {
   try {
     console.log("Sending email to:", to);
-    console.log("EMAIL_USER:", process.env.EMAIL_USER);
-    console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -69,9 +105,9 @@ const sendEmail = async (to, subject, text, htmlContent) => {
 
     await transporter.sendMail({
       from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
-      to: to,
-      subject: subject,
-      text: text,
+      to,
+      subject,
+      text,
       html: htmlContent
     });
 
