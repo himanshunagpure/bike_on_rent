d5137366fc6b99ec038e2c3941cc94191f4b8d79
Himanshu <nagpurehimanshu9@gmail.com>
2026-03-29 23:02:27 +0530

    switch to resend for email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -87,32 +87,58 @@
 
 
 
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (to, subject, text, htmlContent) => {
+//   try {
+//     console.log("Sending email to:", to);
+
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 587,
+//       secure: false,
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//       }
+//     });
+
+//     await transporter.sendMail({
+//       from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//       html: htmlContent
+//     });
+
+//     console.log("Email sent successfully to:", to);
+
+//   } catch (err) {
+//     console.error("sendEmail FAILED:", err.message);
+//   }
+// };
+
+// export default sendEmail;
+
+
+
+
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text, htmlContent) => {
   try {
     console.log("Sending email to:", to);
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
-
-    await transporter.sendMail({
-      from: `"BIKEONRENT 🚴" <${process.env.EMAIL_USER}>`,
+    await resend.emails.send({
+      from: "BIKEONRENT <onboarding@resend.dev>",
       to,
       subject,
       text,
       html: htmlContent
     });
-
-    console.log("Email sent successfully to:", to);
-
+    console.log("Email sent successfully!");
   } catch (err) {
     console.error("sendEmail FAILED:", err.message);
   }
